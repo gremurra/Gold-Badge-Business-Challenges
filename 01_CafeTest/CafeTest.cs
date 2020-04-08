041e3bd67a2cc59273d991556484b139184db6cf
Greg Murray <gremurra@gmail.com>
2020-04-08 15:06:13 -0400

    two challenges done

---
+++ 01_CafeTest/CafeTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using _01_CafeLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _01_CafeTest
+{
+
+    /*The Menu Items:
+A meal number, so customers can say "I'll have the #5"
+A meal name
+A description
+A list of ingredients,
+A price
+
+Your Task:
+Create a Menu Class with properties, constructors, and fields.
+Create a MenuRepository Class that has methods needed.
+Create a Test Class for your repository methods. (You don't need to test your constructors or objects, just your methods)
+Create a Program file that allows the cafe manager to add, delete, and see all items in the menu list.
+
+Notes:
+We don't need to be able to update items right now.*/
+
+
+    [TestClass]
+    public class CafeTest
+    {
+        private Menu _item;
+        private MenuRepository _repo;
+
+        [TestInitialize]
+        public void Arrange()
+        {
+            _repo = new MenuRepository();
+            _item = new Menu(1, "cheeseburger", "Our spin on the All-American Classic, with fries and drink included.", new List<string> { "bun", "patty", "cheese", "lettuce", "tomato", "pickle" }, 4.99m );
+            _repo.AddMenuItem(_item);
+        }
+
+        [TestMethod]
+        public void AddMenuItem_ShouldReturnAreEqual()
+        {
+            //Arrange       //adding the item up above in TestInitialize, so no need to do it again here
+            //_repo.AddMenuItem(_item);
+
+            //Act
+            int expected = 1;
+            int actual = _repo.DisplayMenuItems().Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetMenuItemByName_ShouldReturnCorrectItem()
+        {
+            Menu searchedItem = _repo.GetMenuItemByName("cheeseburger");
+            Assert.AreEqual("cheeseburger", searchedItem.MealName);
+        }
+
+        [TestMethod]
+        public void DeleteMenuItem_ShouldReturnAreEqual()
+        {
+            //Arrange
+            _repo.DeleteMenuItem(_item);
+
+            //Act
+            int expected = -1;
+            int actual = _repo.DisplayMenuItems().Count;
+
+            //Assert
+            Assert.AreNotEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DisplayMenuItems_ShouldReturnFullMenuList()
+        {
+            //Arrange
+            Menu testItem = new Menu();
+            MenuRepository testRepo = new MenuRepository();
+            testRepo.AddMenuItem(testItem);
+
+            //Act
+            List<Menu> testMenu = testRepo.DisplayMenuItems();
+            bool menuHasItems = testMenu.Contains(testItem);
+
+            //Assert
+            Assert.IsTrue(menuHasItems);
+        }
+    }
+}
---
+++ 01_CafeUI/ProgramUI.cs
@@ -0,0 +1,152 @@
+﻿using _01_CafeLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_CafeUI
+{
+    /*The Menu Items:
+A meal number, so customers can say "I'll have the #5"
+A meal name
+A description
+A list of ingredients,
+A price
+
+Your Task:
+Create a Menu Class with properties, constructors, and fields.
+Create a MenuRepository Class that has methods needed.
+Create a Test Class for your repository methods. (You don't need to test your constructors or objects, just your methods)
+Create a Program file that allows the cafe manager to add, delete, and see all items in the menu list.
+
+Notes:
+We don't need to be able to update items right now.*/
+    class ProgramUI
+    {
+        private MenuRepository _repo = new MenuRepository();
+
+        public void Run()
+        {
+            SeedMenuList();
+            RunMenu();
+        }
+
+        private void RunMenu()
+        {
+            bool continueToRun = true;
+            while (continueToRun)
+            {
+                Console.Clear();
+                Console.WriteLine("Please select the option you'd prefer: \n" +
+                    "1. Display whole menu \n" +
+                    "2. Add new menu item \n" +
+                    "3. Delete existing menu item \n" +
+                    "4. Exit");
+                string userInput = Console.ReadLine();
+                switch (userInput)
+                {
+                    case "1":
+                        {
+                            //Display all menu items
+                            DisplayMenuItems();
+                            break;
+                        }
+                    case "2":
+                        {
+                            //Add new menu item
+                            AddMenuItem();
+                            break;
+                        }
+                    case "3":
+                        {
+                            //Delete existing menu item
+                            DeleteMenuItem();
+                            break;
+                        }
+                    case "4":
+                        {
+                            //Exit
+                            continueToRun = false;
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Please select an appropriate option (1-4)");
+                            break;
+                        }
+                }
+            }
+        }
+
+        private void DisplayMenuItems()
+        {
+            List<Menu> menuItems = _repo.DisplayMenuItems();
+            foreach (Menu item in menuItems)
+            {
+                Console.WriteLine($"{item.MealNumber}. {item.MealName}");
+                Console.WriteLine($"Description: {item.MealDescription}");
+                Console.Write("Ingredients: ");
+                foreach (string ingredient in item.ListOfIngredients)
+                {
+                    Console.Write($"{ingredient}, ");
+                }
+                Console.WriteLine(" \n");
+                Console.WriteLine($"${item.Price}");
+                Console.WriteLine(" \n" +
+                    "------------");
+            }
+            Console.WriteLine("Press any key to return to the main screen...");
+            Console.ReadKey();
+        }
+
+        private void AddMenuItem()
+        {
+            Menu itemAdded = new Menu();
+            Console.Write("Please enter a new meal number: ");
+            string mealNumber = Console.ReadLine();
+            itemAdded.MealNumber = int.Parse(mealNumber);
+
+            Console.Write("Please enter the name of the new meal: ");
+            itemAdded.MealName = Console.ReadLine();
+
+            Console.Write("Please describe the new meal: ");
+            itemAdded.MealDescription = Console.ReadLine();
+
+            Console.Write("Please enter the ingredients in the new meal: ");
+            itemAdded.ListOfIngredients = new List<string> { Console.ReadLine() };
+
+            Console.Write("Please enter the price of the new meal: ");
+            string priceOfMeal = Console.ReadLine();
+            itemAdded.Price = decimal.Parse(priceOfMeal);
+
+            _repo.AddMenuItem(itemAdded);
+            Console.WriteLine("Press any key to return to the main screen...");
+            Console.ReadKey();
+        }
+
+        private void DeleteMenuItem()
+        {
+            Console.WriteLine("Please enter the name of the menu item you wish to delete: ");
+            Menu itemDeleted = _repo.GetMenuItemByName(Console.ReadLine());
+
+            _repo.DeleteMenuItem(itemDeleted);
+            Console.WriteLine("Press any key to return to the main screen...");
+            Console.ReadKey();
+        }
+
+
+        private void SeedMenuList()
+        {
+            Menu cheeseburger = new Menu(1, "Cheeseburger", "Our spin on the All-American Classic, with steak fries and drink included.", new List<string> { "bun", "patty", "cheese", "lettuce", "tomato", "pickle" }, 4.99m);
+            Menu chickensandwich = new Menu(2, "Chicken Sandwich", "Fried chicken on a bun, with waffle fries and drink included.", new List<string> { "bun", "chicken", "pickle"}, 4.99m);
+            Menu hotdog = new Menu(3, "Hotdog", "All-beef hotdog stuck between two sesame seed buns, just like you are at the ballpark.", new List<string> { "hotdog bun", "hotdog" }, 3.99m);
+            Menu cheesecake = new Menu(4, "Cheesecake", "One slice of New York-Style cheesecake with a chocolate drizzle on top.", new List<string> { "cheesecake", "chocolate sauce" }, 3.99m);
+
+            _repo.AddMenuItem(cheeseburger);
+            _repo.AddMenuItem(chickensandwich);
+            _repo.AddMenuItem(hotdog);
+            _repo.AddMenuItem(cheesecake);
+        }
+    }
+}
